7b0a92c684dd62ce97cc98ded13ed2e0fc692dd4
justbouton <justb1985@gmail.com>
2020-03-27 19:41:08 -0700

    File: earthquake.js - Updated Pseudocode throughout. Replaced constructor function with let attributes of new function to hold attribute data of API response. Completed compare function for parsedEarthquakeData and earthquakeDB. Relocated Earthquake.created from API call into storeEarthquakeData function for use with compareEarthquakeData. Everything is looking good!

--- earthquake.js
+++ earthquake.js
@@ -1,9 +1,10 @@
 const axios = require('axios');
 const Earthquake = require('./models/earthquakeSchema');
-// Natral disaster trigger
-// Every x minutes call API to see if there has been an earthquake. If an earthquake of x magnitude, capture to earthquake DB.
 
-// List of earthquake objects
+
+// Every "x" minutes call API to see if there has been an earthquake. If an earthquake occured of "y" magnitude, capture to array. If any of the earthquakes from the array are "z" minutes old, push to earthquake DB, alert admin.
+
+// API call for data
 var parsedEarthquakeData = [];
 
 // Recursive function with time delay
@@ -21,45 +22,43 @@ function getEarthquakeData() {
     // https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&latitude=37.773972&longitude=-122.431297&maxradiuskm=100&orderby=time
 
     // API url to earthquake.usgs.gov
-    const url = `https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&latitude=${latitude}&longitude=${longitude}&maxradiuskm=${radius}&orderby=time`
+    const url = `https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&latitude=${latitude}&longitude=${longitude}&maxradiuskm=${radius}&orderby=time&limit=7`
 
-    // axios call to API
+    // axios call to API to gather then parse earthquake data
     axios.get(url)
         .then((data) => {
-            // Earthquake constructor
-            function Earthquake(id, time, place, url, mag) {
-                this.id = id;
-                this.time = time;
-                this.place = place;
-                this.url = url;
-                this.mag = mag;
-                //parsedEarthquakeData.push(this); 
-                this.pushToTheList = () => parsedEarthquakeData.push(this);
-                this.pushToTheList();
-            }
-            //  
+
+            // Parse data
             data = data.data.features
 
-            // Loop through each response i to parse time, place, url, mag, store in temp array
-            const respLength = data.length
-            for (let i = 0; i < respLength; i++) {
-                // Declare var response to store earthquakes data from data[i].properties and .id
-                const response = data[i].properties
-                const id = data[i].id
-                // ES6 destructuring to parse out; time, place, url, mag
-                const { time, place, url, mag } = response;
-                // Using Earthquake constructor push to parsedEarthquakeData
-                new Earthquake(id, time, place, url, mag)
-            }; 
+            // For each earthquake alert pull the element id, time, place, url, mag
+            data.forEach(element => {
+                let { id } = element
+                let { time, place, url, mag } = element.properties
 
-            compareEarthquakeData()
+                // Wrap the attributes
+                let attributes = new function() {
+                    this.id = id;
+                    this.time = time;
+                    this.place = place;
+                    this.url = url;
+                    this.mag = mag;
+                  };
+                  // Push alert attributes to parsedEarthquakeData
+                  parsedEarthquakeData.push(attributes)
+            });
+
+            // console.log(parsedEarthquakeData)
+            
+            // Compare the API data to the DB
+            compareEarthquakeData();
+
         })
         .catch((err) => console.log("API call error - " + err));
 
     // setTimeout(() => getEarthquakeData(), 300000)//300000 = 5 minutes
 };
 
-getEarthquakeData();
 
 
 var earthquakeCount = 0;
@@ -68,66 +67,117 @@ function compareEarthquakeData() { // TEST partialy working
 
 // Pull all DB earthquake data to local array. Compare that array to API parsedEarthquakeData
 
-    // Get earthquake count
+    // Check to see if any earthquakes exits in DB
     Earthquake.countDocuments({}, function (err, count) {
         earthquakeCount = count;
     }).then(() => {
-        // if earthquakeCount === 0 log no data. Else loop through each then Earthquake.create
+        // if earthquakeCount === 0 log no data. Else Earthquake.create
         if (earthquakeCount === 0) {
             console.log("no earthquake data")
-            // Add data to Earthquake DB CREATE FUNCTION
-                parsedEarthquakeData.forEach(element => {
-console.log("Adding ID: " + element.id + " to db.earthquake")     
-                    // ES6 destructuring to parse out; time, place, url, mag
-                    const { id, time, place, url, mag } = element;
-
-                    Earthquake.create({
-                            id: id,
-                            time : time,
-                            place : place,
-                            url : url,
-                            mag : mag
-                    })
-                })
+            // Store earthquake data to DB.
+            storeEarthquakeData(parsedEarthquakeData)
         } else {
-//             // compare to Earthquake DB
+
             console.log("Woohoo! Found earthquake data.")
-//             // Check Earthquake DB against parsedEarthquakeData by id add to DB if not present
-//             let earthquakeDB = []
-//             // Pull Earthquake DB data place in array
-//             Earthquake.find({}, function (err, res){
-//                 res.forEach(element => {
-//                     earthquakeDB.push(element)
-//                 });
 
-//             let earthquakeDifferences = [];
+            // Check earthquakeDB against API response parsedEarthquakeData. Compare id if already in earthquakeDB pop off that id else push to earthquakeDifferences.
 
-//             // Loop through each earthquakeDB.id and compare with parsedEarthquakeData.id
-//             for (var i = 0; i < earthquakeCount; i++) {
-//                 let quakeid = earthquakeDB[i].id
-// console.log("forLoop " + i)
+            let earthquakeDB = [];
+            let earthquakeDifferences = [];
+            
+            // Pull Earthquake DB data place in array
+            Earthquake.find({}, function (err, res){
+                res.forEach(element => {
+                    earthquakeDB.push(element)
+                // console.log(element)
+            });
 
-//                 parsedEarthquakeData.forEach(element => {
-//                     let parsedid = element.id
-//                     // compare quakeid to parsdid
-//                     // console.log(parsedid)
-                    
-//                     // Compared Earthquake ID if no match add to Earthquake DB
-//                     function compareEarthquakeID() {
-//                         if (quakeid !== parsedid) {
-// console.log("NO MATCH adding incident to earthquakeDB: " + element)
-//                             // Add to Earthquake DB using parsedid
-//                             // Earthquake.create({
-                                
-//                             // })
-//                         }
-//                     }
-//                     compareEarthquakeID()
-//                 });
-                
-//                 // // if id does not match add to Earthquake DB and trigger SMS admin                
-//             }
-//             })
+
+            // console.log("earthquakeDB")
+            // console.log(earthquakeDB)
+            // console.log("parsedEarthquakeData")
+            // console.log(parsedEarthquakeData)
+            var props = ['id', 'time', 'place', 'url', 'mag'];
+
+            var result = parsedEarthquakeData.filter(function(o1){
+                // filter out (!) items in earthquakeDB
+                return !earthquakeDB.some(function(o2){
+                    return o1.id === o2.id;          // assumes unique id
+                });
+            }).map(function(o){
+                // use reduce to make objects with only the required properties
+                // and map to apply this to the filtered array as a whole
+                return props.reduce(function(newo, name){
+                    newo[name] = o[name];
+                    return newo;
+                }, {});
+            });
+            
+            console.log(result)
+            storeEarthquakeData(result)
+            });
+
         }
     }).catch((err) => console.log("compareEarthquakeData error: " + err));
 };
+
+// Store earthquake data to database
+function storeEarthquakeData(arr) {
+    // Add data to Earthquake DB 
+    arr.forEach(element => {
+console.log("Adding ID: " + element.id + " to db.earthquake")     
+        // ES6 destructuring to parse out; id, time, place, url, mag
+        const { id, time, place, url, mag } = element;
+
+        Earthquake.create({
+                id: id,
+                time : time,
+                place : place,
+                url : url,
+                mag : mag
+        })
+    })
+};
+
+// // getEarthquakeData -> compareEarthquakeData -> storeEarthquakeData
+getEarthquakeData();
+
+
+
+
+
+//              // Loop through each earthquakeDB.id and compare with parsedEarthquakeData.id
+//              for (var i = 0; i < earthquakeCount; i++) {
+//                 let quakeid = earthquakeDB[i].id
+// // console.log("forLoop " + i)
+
+//                 parsedEarthquakeData.forEach(element => {
+//                     let parsedid = element.id
+// // console.log(parsedid)
+//                 // Compared quakeid to parsdid if no match add to earthquakeDifferences
+//                         if (parsedid === quakeid) {
+//                             // pop from parsedEarthquakeData
+//                         } else {
+//                             // push to earthquakeDifferences
+//                         }          
+
+//                 // Push earthquakeDifferences to db.earthquakes.
+
+
+
+// // // console.log("NO MATCH adding incident to earthquakeDB: " + parsedid)
+// //                                 // Add to Earthquake DB to earthquakeDifferences using parsedid
+// //                                 // earthquakeDifferences.push(parsedid)
+// //     //                             // Earthquake.create({
+//                     })
+//                 }
+//                 // });
+                
+// //     //                 // // if id does not match add to Earthquake DB and trigger SMS admin                
+// //                 console.log("earthquakeDifferences")
+// //                 console.log(earthquakeDifferences)
+//         })
+
+
+
+
